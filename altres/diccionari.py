46bda8086f340a5b8d73a7b71c04f2fcc9dc5b1f
Joan <joanmam@gmail.com>
2025-04-10 09:39:06 +0200

    Pas 154. actualitzacio diccionari

--- altres/diccionari.py
+++ altres/diccionari.py
@@ -1,3 +1,5 @@
 diccionari_imatges = {
-    "jamieoliver.com": "https://i.postimg.cc/4x6JmQyk/img.png"
+    "jamieoliver.com": "https://i.postimg.cc/4x6JmQyk/img.png",
+    "nyt.com" : "https://i.postimg.cc/6TsTz8rk/img-1.png",
+    "webosfritos.es" : "https://i.postimg.cc/BnmPfn4K/img-2.png"
 }
